486069b25ff011004379fb9f1b37418026309cc9
Divya Ramakrishnan <4divyar@gmail.com>
2026-02-10 15:16:46 -0500

    Added program control test cases

--- src/main/java/ProgramControl.java
+++ src/main/java/ProgramControl.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class ProgramControl {
+    private FileHandler fileHandler;
+    private Cipher cipher; // fill in with cipher class name
+
+    // constructor
+    public ProgramControl(FileHandler fileHander, Cipher cipher) {
+        this.fileHandler = fileHander;
+        this.cipher = cipher;
+    }
+
+    // main method that returns the correct content
 }
