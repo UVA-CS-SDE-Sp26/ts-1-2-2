60890435fb8e358d8eac1d9e6389822868473687
JasonThai198 <jasonthai2005@outlook.com>
2026-02-09 21:14:16 -0500

    Creating FileHandler Java Class, Created some test cases for FileHandler Methods.

---
+++ src/main/java/FileHandler.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class FileHandler {
+    String dataFilePath;
+    public List<String> listFiles(){
+        return null;
+    }
+    
+    public String readFile(String fileName) {
+        return null;
+    }
+}
--- test/java/FileHandlerTest.java
+++ test/java/FileHandlerTest.java
@@ -1,2 +1,34 @@
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class FileHandlerTest {
+    @Test
+    void listFilesReturnsAllFilesInDirectory() {
+        FileHandler fileHandler = new FileHandler();
+        List<String> files = fileHandler.listFiles();
+        assertTrue(files.contains("carnivore.txt"));
+        assertTrue(files.contains("cointelpro.txt"));
+    }
+
+    @Test
+    void readFilesReturnsCorrectContents() {
+        FileHandler fileHandler = new FileHandler();
+        String contains = fileHandler.readFile("carnivore.txt");
+        assertTrue(contains.contains("Carnivore, later renamed DCS1000, was a system implemented by the Federal Bureau of Investigation (FBI) that was\n" +
+                "designed to monitor email and electronic communications. It used a customizable packet sniffer that could monitor all\n" +
+                "of a target user's Internet traffic. Carnivore was implemented in October 1997. By 2005 it had been replaced with\n" +
+                "improved commercial software."));
+    }
+
+    @Test
+    void readFilesThrowsExceptionWhenFileDoesNotExist() {
+        FileHandler fileHandler = new FileHandler();
+        String contains = fileHandler.readFile("nothere.txt");
+        assertThrows(IOException.class, () -> {
+            fileHandler.readFile("nothere.txt");
+        });
+    }
+
 }
