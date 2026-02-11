ef3506022fdc23704f2b4fbef202634b7d39f9fb
chinmayeeyerraguntla <chinmayeeyerraguntla.uva@gmail.com>
2026-02-11 11:21:25 -0500

    added user interface object to TopSecret

--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -7,8 +7,6 @@ public class TopSecret {
     public  static void main(String[] args) {
         UserInterface userInterface = new UserInterface();
         String result = userInterface.parseArgs(args);
-
-
     }
 
 
