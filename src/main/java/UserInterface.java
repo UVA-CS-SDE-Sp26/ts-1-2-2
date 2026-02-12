76a766b3a72fec189d1143f488806b30570897f1
chinmayeeyerraguntla <chinmayeeyerraguntla.uva@gmail.com>
2026-02-12 16:34:59 -0500

    updated user interface class

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -3,32 +3,23 @@ public class UserInterface {
     public String parseArgs(String[] args){
         String result = "";
         if(args.length == 0){
-            result =  "Listing available files to decipher: ";
+            return  "Listing available files to decipher: ";
             // call file handler to do this
         }
+        else if(!args[0].matches("\\d+")){
+            return args[0] + " is not a valid file input; it must be an number";
+        }
         else if(args.length == 1){
-            if(!args[0].matches("\\d+")){
-                result = args[0] + " is not a valid file input; it must be an number";
-            }
-            else{
-                result = "Printing file "+args[0]+" using the default key";
-                // call file handler
-            }
+            return "Printing file "+args[0]+" using the default key";
+            // call file handler
 
         }
         else if(args.length == 2){
-            if(!args[0].matches("\\d+")){
-                result = args[0] + " is not a valid file input; it must be an number";
-            }
-            else{
-                result =  "Printing file "+args[0]+" using the " +args[1]+" key";
-                // call file handler
-            }
+            return  "Printing file "+args[0]+" using the " +args[1]+" key";
         }
         else{
-            result =  "Error: The user may only provide up to two arguments: a file number and an alternate key";
+            return  "Error: The user may only provide up to two arguments: a file number and an alternate key";
         }
-        return result;
     }
 
 
