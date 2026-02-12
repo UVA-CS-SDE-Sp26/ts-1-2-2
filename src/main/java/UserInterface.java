d877e52c50f592083f6d998ed49e35197484fc17
chinmayeeyerraguntla <chinmayeeyerraguntla.uva@gmail.com>
2026-02-12 16:50:40 -0500

    updated user interface class

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -1,17 +1,14 @@
 
 public class UserInterface {
     public String parseArgs(String[] args){
-        String result = "";
         if(args.length == 0){
             return  "Listing available files to decipher: ";
-            // call file handler to do this
         }
         else if(!args[0].matches("\\d+")){
             return args[0] + " is not a valid file input; it must be an number";
         }
         else if(args.length == 1){
             return "Printing file "+args[0]+" using the default key";
-            // call file handler
 
         }
         else if(args.length == 2){
